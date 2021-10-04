b897bf888cb194a7cd0088e82849bcfb4e0ed3e8
Rune Bjerke <github@green.spoox.org>
2021-10-04 13:40:52 +0200

    add dev loader

---
+++ WayfarerQOL.DEV.user.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name         WayfarerQOL (dev loader)
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Wayfarer QOL improvements
+// @author       sp00x
+// @match        https://wayfarer.nianticlabs.com/*
+// @grant        none
+// @require      file:///C:/source/js/WayfarerQOL/WayfarerQOL.user.js
+// ==/UserScript==
